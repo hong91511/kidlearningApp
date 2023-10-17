e6b54c1ad1e999a9882ac0d44df6aa15e8dab4df
Student <Student@205-21>
2023-10-17 15:26:17 +0800

    加入專案檔案。

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() 
+        { 
+        InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
